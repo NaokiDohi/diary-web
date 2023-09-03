9e2e13906d121e77669e90cd9cd5e3a64aaa2024
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2023-09-04 08:21:34 +0900

    Fix butg about EventComponent useing chakra-ui/crad

--- components/Cards/EventCard.tsx
+++ components/Cards/EventCard.tsx
@@ -1,4 +1,12 @@
-import { Box } from '@chakra-ui/react'
+import {
+  Text,
+  Box,
+  Heading,
+  Stack,
+  Card,
+  CardHeader,
+  CardBody,
+} from '@chakra-ui/react'
 import { EventType } from '../../types/event'
 
 type EventPropsType = {
@@ -15,24 +23,33 @@ const EventCard = ({ event }: EventPropsType) => {
     .split(' ')[4]
     .split(':')
   return (
-    // <Box className='col rounded bg-green-500 m-5 shadow-lg'>
-    //   <div className={`card m-5 rounded-3 shadow-sm`}>
-    //     <div className={`card-header py-3`}>
-    //       <h1 className='my-0 fw-normal'>・{event.title}</h1>
-    //     </div>
-    //     <div className='card-body mb-5'>
-    //       <h1 className='card-title pricing-card-title'>
-    //         ・{event.description}
-    //       </h1>
-    //       <ul className='list-unstyled mt-3 mb-5'>
-    //         <li>・{`${startH}:${startM}`}</li>
-    //         <li>・{`${endH}:${endM}`}</li>
-    //       </ul>
-    //       {/* <pre>{JSON.stringify(event, null, 4)}</pre> */}
-    //     </div>
-    //   </div>
-    // </Box>
-    <Card size={'lg'}></Card>
+    <Card minW='250px' className='rounded bg-green-500 m-1 shadow-lg'>
+      <CardHeader textAlign='left'>
+        <Heading size='md' className='card-header'>
+          {event.title}
+        </Heading>
+      </CardHeader>
+      <CardBody textAlign='left'>
+        <Stack spacing='1'>
+          <Box>
+            <Text pt='2' fontSize='sm'>
+              {event.description}
+            </Text>
+          </Box>
+          <Box>
+            <Heading size='xs' textTransform='uppercase'>
+              ・{`${startH}:${startM}`}
+            </Heading>
+          </Box>
+          <Box>
+            <Heading size='xs' textTransform='uppercase'>
+              ・{`${endH}:${endM}`}
+            </Heading>
+          </Box>
+        </Stack>
+      </CardBody>
+      {/* <pre>{JSON.stringify(event, null, 4)}</pre> */}
+    </Card>
   )
 }
 
